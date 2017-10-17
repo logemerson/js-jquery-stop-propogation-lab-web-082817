bb8abf005adada0fca1f95a21f1ab3636aefd84d
github username <logemerson@gmail.com>
2017-10-17 17:22:17 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {
-
   lightActive();
   redLight();
   yellowLight();
   greenLight();
-
-  
 });
 
-function lightActive(){
-  $('.light').on('click', function(){
-      $(this).toggleClass("active");
-      console.log("background purple")
+function lightActive() {
+  $(".light").on("click", function(event) {
+    $(this).toggleClass("active");
+    console.log("background purple");
   });
 }
 
-function redLight(){
-  $(".redLight").on('click', function(){
+function redLight() {
+  $(".redLight").on("click", function() {
     $(this).toggleClass("red");
+    event.stopPropagation();
   });
 }
 
-
-function yellowLight(){
-  $(".yellowLight").on('click', function(){
+function yellowLight() {
+  $(".yellowLight").on("click", function() {
     $(this).toggleClass("yellow");
+    event.stopPropagation();
   });
 }
 
-function greenLight(){
-  $(".greenLight").on('click', function(){
+function greenLight() {
+  $(".greenLight").on("click", function() {
     $(this).toggleClass("green");
+    event.stopPropagation();
   });
 }
-
